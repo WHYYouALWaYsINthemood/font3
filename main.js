949d730a8fd0ec7af465153fbcea35b8156a5b0b
WHYYouALWaYsINthemood <122185449+WHYYouALWaYsINthemood@users.noreply.github.com>
2024-02-09 08:47:53 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550, 550);
+    canvas.position(600, 100);   
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('poseNet is initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+    }
+}
+
+function draw()
+{
+ background('#ADD8E6');
+    document.getElementById("square_side").innerHTML = "width and height of a square will be = " + difference + "px";
+    fill('#FF7F7F');
+    stroke('#FF7F7F');
+    square(noseX, noseY, difference);
+}
